97e8439dce0e1adc6e8dce2301d5f71f4550c51b
islam-ahsanul <ahsanulislam.info@gmail.com>
2024-09-07 11:22:08 +0600

    Refactor home page to display a styled welcome message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center mt-20 font-extrabold text-3xl ">
+      <p>
+        Welcome to{' '}
+        <span className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-700 bg-clip-text text-transparent">
+          BUG TRACKER
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Home;
