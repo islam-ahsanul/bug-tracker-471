e0814ffbb6f0653b4113bf834af6aae97eaa9799
islam-ahsanul <ahsanulislam.info@gmail.com>
2024-09-11 09:16:31 +0600

    db push command

--- app/api/admin/projects/route.ts
+++ app/api/admin/projects/route.ts
@@ -5,8 +5,8 @@ import { auth } from '@/utils/auth';
 export const POST = async (request: Request) => {
   const { name, description } = await request.json();
   const session = await auth();
-  
-  if (!session || session.user?.roles !== 'ADMIN') {
+
+  if (!session || session.user?.role !== 'ADMIN') {
     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
   }
 
@@ -18,8 +18,14 @@ export const POST = async (request: Request) => {
       },
     });
 
-    return NextResponse.json({ message: 'Project created successfully', project }, { status: 201 });
+    return NextResponse.json(
+      { message: 'Project created successfully', project },
+      { status: 201 }
+    );
   } catch (error) {
-    return NextResponse.json({ message: 'Something went wrong', errorLog: error }, { status: 500 });
+    return NextResponse.json(
+      { message: 'Something went wrong', errorLog: error },
+      { status: 500 }
+    );
   }
 };
