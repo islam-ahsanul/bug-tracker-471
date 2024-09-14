1a6cfc64f7fa31dc6cdd2e3ab2b50bfdf211d8f8
islam-ahsanul <ahsanulislam.info@gmail.com>
2024-09-14 08:26:43 +0600

    developer layout

---
+++ app/(dashboard)/developer/layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { auth } from '@/utils/auth';
+
+import TopBar from '@/components/dasboards/TopBar';
+import LeftSidebar from '@/components/dasboards/developer/LeftSidebar';
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+  return (
+    <main className="flex flex-row">
+      {session?.user.role === 'DEVELOPER' ? (
+        <>
+          <TopBar />
+          <LeftSidebar />
+          <section className="main-container">
+            <div className="w-full max-w-5xl">{children}</div>
+          </section>
+        </>
+      ) : (
+        <div>You are not authorized</div>
+      )}
+    </main>
+  );
+};
+
+export default layout;
