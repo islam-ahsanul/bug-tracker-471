de1081964860bd98ac66cf936d373a7ad9b6d290
Aunanna-Binte-Wahid <165040137+Aunanna-Binte-Wahid@users.noreply.github.com>
2024-09-14 14:23:36 +0600

    console log

--- components/profile/profileData.tsx
+++ components/profile/profileData.tsx
@@ -2,6 +2,7 @@ import ResetPasswordButton from './ResetPasswordButton';
 import DeleteAccountButton from './DeleteAccountButton';
 
 const ProfileData = ({ user }: { user: any }) => {
+   // console.log(session);
   return (
     <div className="flex flex-col gap-4 justify-center items-center w-[50%] py-20 border border-gray-300/50 rounded-3xl shadow-lg shadow-gray-200">
       <h1 className="text-4xl font-bold">Welcome, {user.name || 'User'}</h1>
